3c68dab01b0bbade98c5e082a3e8c71ab1675e9c
jose-valdez_IMSSB <jose-valdez_IMSSB@users.noreply.github.com>
2026-07-15 18:10:00 -0600

    acomodo de front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,16 +7,16 @@ export default defineConfig({
   server: {
     proxy: {
       '/django': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/django/, ''),
       },
       '/fision': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8001',
         changeOrigin: true,
       },
       '/principal': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8001',
         changeOrigin: true,
       },
     },
